eb9f9f5fddceef566a124e8d2e8ca2a67e086145
Miguel Lara <miguel.larasj@gmail.com>
2023-10-29 16:57:00 -0500

    initial commit

---
+++ bin/evaluacion_miguel_lara_2.dart
@@ -0,0 +1,5 @@
+import 'package:evaluacion_miguel_lara_2/evaluacion_miguel_lara_2.dart' as evaluacion_miguel_lara_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${evaluacion_miguel_lara_2.calculate()}!');
+}
---
+++ test/evaluacion_miguel_lara_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:evaluacion_miguel_lara_2/evaluacion_miguel_lara_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
